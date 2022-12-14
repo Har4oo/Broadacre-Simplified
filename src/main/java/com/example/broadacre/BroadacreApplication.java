cc75b700f8051caa6483ff65a8b0cc2e9ab7fcbb
Har4oo <harihharalampiev@gmail.com>
2022-12-14 11:35:02 +0200

    Initial commit

---
+++ src/main/java/com/example/broadacre/BroadacreApplication.java
@@ -0,0 +1,13 @@
+package com.example.broadacre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadacreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroadacreApplication.class, args);
+	}
+
+}
