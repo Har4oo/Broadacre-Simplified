1fb4d68d80b1564415f7e1c33b5713656e395200
Har4oo <harihharalampiev@gmail.com>
2023-01-16 11:08:49 +0200

    Creating location

--- src/main/java/com/harvestmanager/broadacre/controller/view/LocationViewController.java
+++ src/main/java/com/harvestmanager/broadacre/controller/view/LocationViewController.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +26,10 @@ public class LocationViewController {
     public String createLocation(Model model){
         model.addAttribute("location",new Location());
         return "/location/createLocation";
+    }
+    @PostMapping("/post/createLocation")
+    public String createLocation(@ModelAttribute Location location){
+        locationService.createLocation(location);
+        return "redirect:/location";
     }
 }
