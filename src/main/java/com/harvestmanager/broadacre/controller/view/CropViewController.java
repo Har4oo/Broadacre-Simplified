9861424197294564d92c53a879ac3fec6b8f14d6
Har4oo <harihharalampiev@gmail.com>
2023-01-17 17:18:43 +0200

    View Controller Implementation

--- src/main/java/com/harvestmanager/broadacre/controller/view/CropViewController.java
+++ src/main/java/com/harvestmanager/broadacre/controller/view/CropViewController.java
@@ -13,38 +13,44 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class CropViewController {
     private CropService cropService;
+    private SoilService soilService;
+    private PlantStageService plantStageService;
+    private SortService sortService;
+    private LocationService locationService;
 
-    @GetMapping
-    public String cropView(Model model){
-        System.out.println("all the crops: "+cropService.getCrops().toString());
-        model.addAttribute("crops",cropService.getCrops());
-        return "/index";
-    }
     @GetMapping("/createCrop")
     public String createCrop(Model model){
         System.out.println("going to create crop html");
         model.addAttribute("crop",new Crop());
+        model.addAttribute("soils", soilService.getSoils());
+        model.addAttribute("sorts", sortService.getSorts());
+        model.addAttribute("locations", locationService.getLocations());
+        model.addAttribute("plantStages", plantStageService.getPlantStages());
         System.out.println(model.toString());
         return "/crop/createCrop";
     }
     @PostMapping("/post/createCrop")
     public String createCrop(@ModelAttribute Crop crop){
-        System.out.println("created: "+crop);
-        System.out.println("sort created");
+        System.out.println("created crop: "+crop);
+        System.out.println("crop created");
         cropService.createCrop(crop);
-        return "redirect:/index";
+        return "redirect:/";
     }
     @GetMapping("/editCrop/{id}")
     public String editSort(Model model, @PathVariable long id){
         System.out.println("crop edit html");
         model.addAttribute("crop",cropService.getCrop(id));
+        model.addAttribute("soils", soilService.getSoils());
+        model.addAttribute("sorts", sortService.getSorts());
+        model.addAttribute("locations", locationService.getLocations());
+        model.addAttribute("plantStages", plantStageService.getPlantStages());
         return "/crop/editCrop";
     }
     @PostMapping("/put/updateCrop/{id}")
     public String updateCrop(@PathVariable long id, @ModelAttribute Crop crop){
         System.out.println("updating value");
         cropService.updateCrop(crop,id);
-        return "redirect:/index";
+        return "redirect:/";
     }
     @GetMapping("/deleteCrop/{id}")
     public String deleteCrop(@PathVariable long id){
