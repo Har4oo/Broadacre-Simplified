3f38356c05afbc5a38def2cc2d639d37bdd74c11
Har4oo <harihharalampiev@gmail.com>
2023-01-30 17:21:53 +0200

    View Controller Implementation

---
+++ src/main/java/com/harvestmanager/broadacre/controller/view/PersonalObservationViewController.java
@@ -0,0 +1,58 @@
+package com.harvestmanager.broadacre.controller.view;
+
+import com.harvestmanager.broadacre.entity.Harvest;
+import com.harvestmanager.broadacre.entity.Location;
+import com.harvestmanager.broadacre.entity.PersonalObservation;
+import com.harvestmanager.broadacre.service.CropService;
+import com.harvestmanager.broadacre.service.HarvestService;
+import com.harvestmanager.broadacre.service.PersonalObservationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/personalObservation")
+@AllArgsConstructor
+public class PersonalObservationViewController {
+    private PersonalObservationService personalObservationService;
+    private CropService cropService;
+
+    @GetMapping
+    public String harvestView(Model model){
+        System.out.println(personalObservationService.getPersonalObservations().toString());
+        model.addAttribute("personalObservations",personalObservationService.getPersonalObservations());
+        model.addAttribute("crops",cropService.getCrops());
+        return "/personalobservation/personalObservation";
+    }
+
+    @GetMapping("/createPersonalObservation")
+    public String createPersonalObservation(Model model){
+        model.addAttribute("personalObservation",new PersonalObservation());
+        model.addAttribute("crops",cropService.getCrops());
+        return "/personalobservation/createPersonalObservation";
+    }
+
+    @PostMapping("/post/createPersonalObservation")
+    public String createPersonalObservation(@ModelAttribute PersonalObservation personalObservation){
+        personalObservationService.createPersonalObservation(personalObservation);
+        return "redirect:/personalObservation";
+    }
+    @GetMapping("/editPersonalObservation/{id}")
+    public String editPersonalObservation(Model model, @PathVariable long id){
+        model.addAttribute("personalObservation",personalObservationService.getPersonalObservation(id));
+        model.addAttribute("crops",cropService.getCrops());
+        return "/personalobservation/editPersonalObservation";
+    }
+    @PostMapping("/put/updateHarvest/{id}")
+    public String updatePersonalObservation(@PathVariable long id, @ModelAttribute PersonalObservation personalObservation){
+        System.out.println("updating value");
+        personalObservationService.updatePersonalObservation(personalObservation,id);
+        return "redirect:/personalObservation";
+    }
+    @GetMapping("/deletePersonalObservation/{id}")
+    public String deletePersonalObservation(@PathVariable long id){
+        personalObservationService.deletePersonalObservation(id);
+        return "redirect:/personalObservation";
+    }
+}
