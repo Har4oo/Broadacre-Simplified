e9619f3a3f282f1b9274512d8a990e7e31fb447d
Har4oo <harihharalampiev@gmail.com>
2023-02-08 14:31:36 +0200

    Removing unnecessary added logic to the business logic

--- src/main/java/com/harvestmanager/broadacre/service/PlantStageService.java
+++ src/main/java/com/harvestmanager/broadacre/service/PlantStageService.java
@@ -10,7 +10,4 @@ public interface PlantStageService {
     PlantStage createPlantStage(PlantStage plantStage);
     PlantStage updatePlantStage(PlantStage plantStage, long id);
     void deletePlantStage(long id);
-
-    int getPlantStageValue(PlantStage plantStage);
-
 }
