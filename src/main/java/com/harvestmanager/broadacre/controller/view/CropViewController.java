ea8f14b0f576f37205a147da04df6f81ccf2f4d7
Har4oo <harihharalampiev@gmail.com>
2023-02-14 16:14:59 +0200

    Fixes for view

--- src/main/java/com/harvestmanager/broadacre/controller/view/CropViewController.java
+++ src/main/java/com/harvestmanager/broadacre/controller/view/CropViewController.java
@@ -18,11 +18,12 @@ public class CropViewController {
     private SortService sortService;
     private LocationService locationService;
     private PersonalObservationService personalObservationService;
+    private HarvestService harvestService;
 
     @GetMapping("/createCrop")
-    public String createCrop(Model model){
+    public String createCrop(Model model) {
         System.out.println("going to create crop html");
-        model.addAttribute("crop",new Crop());
+        model.addAttribute("crop", new Crop());
         model.addAttribute("soils", soilService.getSoils());
         model.addAttribute("sorts", sortService.getSorts());
         model.addAttribute("locations", locationService.getLocations());
@@ -30,38 +31,50 @@ public class CropViewController {
         System.out.println(model.toString());
         return "/crop/createCrop";
     }
+
     @PostMapping("/post/createCrop")
-    public String createCrop(@ModelAttribute Crop crop){
-        System.out.println("created crop: "+crop);
+    public String createCrop(@ModelAttribute Crop crop) {
+        System.out.println("created crop: " + crop);
         System.out.println("crop created");
         cropService.createCrop(crop);
         return "redirect:/";
     }
+
     @GetMapping("/editCrop/{id}")
-    public String editSort(Model model, @PathVariable long id){
+    public String editSort(Model model, @PathVariable long id) {
         System.out.println("crop edit html");
-        model.addAttribute("crop",cropService.getCrop(id));
+        model.addAttribute("crop", cropService.getCrop(id));
         model.addAttribute("soils", soilService.getSoils());
         model.addAttribute("sorts", sortService.getSorts());
         model.addAttribute("locations", locationService.getLocations());
         model.addAttribute("plantStages", plantStageService.getPlantStages());
         return "/crop/editCrop";
     }
+
     @PostMapping("/put/updateCrop/{id}")
-    public String updateCrop(@PathVariable long id, @ModelAttribute Crop crop){
+    public String updateCrop(@PathVariable long id, @ModelAttribute Crop crop) {
         System.out.println("updating value");
-        cropService.updateCrop(crop,id);
+        cropService.updateCrop(crop, id);
         return "redirect:/";
     }
+
     @GetMapping("/deleteCrop/{id}")
-    public String deleteCrop(@PathVariable long id){
+    public String deleteCrop(@PathVariable long id) {
         cropService.deleteCrop(id);
         return "redirect:/";
     }
+
     @GetMapping("/cropDescription/{id}")
-    public String cropDescription(Model model,@PathVariable long id){
-        model.addAttribute("crop",cropService.getCrop(id));
-        model.addAttribute("personalObservations",personalObservationService.getPersonalObservations());
+    public String cropDescription(Model model, @PathVariable long id) {
+        System.out.println("Adding Crop with ID " + cropService.getCrop(id).getCropId() + " to the model");
+        model.addAttribute("crop", cropService.getCrop(id));
+        System.out.println("Adding PersonalObservations to Crop's model with current ID");
+        System.out.println(cropService.getCrop(id).getPersonalObservations());
+        model.addAttribute("personalObservations", cropService.getCrop(id).getPersonalObservations());
+        System.out.println("Adding Harvest to Crop's model with current ID  ");
+        System.out.println(cropService.getCrop(id).getHarvests());
+        model.addAttribute("harvests", cropService.getCrop(id).getHarvests());
+
         return "/crop/cropDescription";
     }
 }
