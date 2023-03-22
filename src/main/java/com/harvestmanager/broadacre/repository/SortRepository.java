878f219f25c4a0bc71aae828d049dacc75ec3786
Har4oo <harihharalampiev@gmail.com>
2023-03-22 11:02:35 +0200

    Repository changes

--- src/main/java/com/harvestmanager/broadacre/repository/SortRepository.java
+++ src/main/java/com/harvestmanager/broadacre/repository/SortRepository.java
@@ -1,7 +1,10 @@
 package com.harvestmanager.broadacre.repository;
 
+import com.harvestmanager.broadacre.entity.CropType;
 import com.harvestmanager.broadacre.entity.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface SortRepository extends JpaRepository<Sort,Long> {
 }
