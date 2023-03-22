af9ac06f7e1ea68c55c5ed56e89ff0ba6d11c18e
Har4oo <harihharalampiev@gmail.com>
2023-03-22 14:31:56 +0200

    CropType view controller additions for update and delete

--- src/main/java/com/harvestmanager/broadacre/controller/view/CropTypeViewController.java
+++ src/main/java/com/harvestmanager/broadacre/controller/view/CropTypeViewController.java
@@ -6,10 +6,7 @@ import com.harvestmanager.broadacre.service.CropTypeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cropType")
@@ -37,5 +34,21 @@ public class CropTypeViewController {
         return "redirect:/cropType";
     }
 
+    @GetMapping("/editCropType/{id}")
+    public String editCropType(Model model, @PathVariable long id){
+        model.addAttribute("cropType",cropTypeService.getCropType(id));
+        return "/cropType/editCropType";
+    }
 
+    @PostMapping("/put/updateCropType/{id}")
+    public String updateLocation(@PathVariable long id, @ModelAttribute CropType cropType){
+        System.out.println("updating value");
+        cropTypeService.updateCropType(cropType,id);
+        return "redirect:/cropType";
+    }
+    @GetMapping("/deleteCropType/{id}")
+    public String deleteCropType(@PathVariable long id){
+        cropTypeService.deleteCropType(id);
+        return "redirect:/cropType";
+    }
 }
